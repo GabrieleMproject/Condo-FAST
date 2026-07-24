97c58ba255e41cb63106aa55e88420f9cba8f276
Gabriele Maesani <maesanigabriele@gmail.com>
2026-07-24 19:02:31 +0200

    S63: Fix crash DashboardPage per import mancante di useNavigate

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import { toast } from 'react-hot-toast'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 import { useCondomini } from '../hooks/useCondomini'
 import { usePlan } from '../hooks/usePlan'
